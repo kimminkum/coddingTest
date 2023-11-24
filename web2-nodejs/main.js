85fafff6754fa6d529ab971fc1fb2f9ace6d5bf4
kimminkum <100018239+kimminkum@users.noreply.github.com>
2023-11-24 17:03:55 +0900

    41ing

--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var qs = require("querystring");
 
 function templateHTML(title, list, body) {
   return `
@@ -13,6 +14,7 @@ function templateHTML(title, list, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
     </p>
   </body>
@@ -63,6 +65,39 @@ var app = http.createServer(function(request, response) {
         });
       });
     }
+  } else if (pathname === "/create") {
+    fs.readdir("./data", (err, filelist) => {
+      var title = "web - create";
+      var list = templateList(filelist);
+      var template = templateHTML(
+        title,
+        list,
+        `<form action="http://localhost:3030/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title" /></p>
+        <p><textarea name="description" id="" cols="30" rows="10" placeholder="description"></textarea></p>
+        <p><input type="submit" /></p>
+      </form>
+      `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathname === "/create_process") {
+    var body = "";
+    request.on("data", data => {
+      body += data;
+      if (body.length > 1e6) {
+        request.destroy();
+      }
+    });
+    request.on("end", () => {
+      var post = parse(body);
+      var title = post.title;
+      var description = post.description;
+      console.log(post.tilte);
+    });
+    response.writeHead(200);
+    response.end("sucess");
   } else {
     response.writeHead(404);
     response.end("Not found");
