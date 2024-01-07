0582ebdbc4e9cd7e9af71dd9c1f6c0866edc65bd
kimminkum <100018239+kimminkum@users.noreply.github.com>
2024-01-07 17:34:05 +0900

    did mount > use Effect , constructor > const useState change

--- react-board/src/App.tsx
+++ react-board/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BoardList from "./BoradList";
@@ -10,6 +10,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
  * App class
  */
 const App: React.FC = () => {
+  const [date, setDate] = useState("");
+
+  const callApi = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => {
+        setDate(json.title);
+      });
+  };
+
+  useEffect(() => {
+    // 컴포넌트가 마운트되면 API 호출
+    callApi();
+  }, []); // 빈 배열은 마운트될 때 한 번만 실행됨
+
+  // {
+  //   "userId": 1,
+  //   "id": 1,
+  //   "title": "delectus aut autem",
+  //   "completed": false
+  // }
+
   return (
     <div className="App">
       <div className="bg"></div>
@@ -22,6 +44,7 @@ const App: React.FC = () => {
             <Route path="/detail/:id" element={<Detail />} />
             <Route path="/" element={<BoardList />} />
           </Routes>
+          <div>{date ? date : "none"}</div>
         </BrowserRouter>
       </div>
     </div>
