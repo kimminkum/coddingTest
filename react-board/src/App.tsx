bd9f419875796c38f813392c09918102faac7f72
kimminkum <100018239+kimminkum@users.noreply.github.com>
2024-01-08 10:04:44 +0900

    api try

--- react-board/src/App.tsx
+++ react-board/src/App.tsx
@@ -1,36 +1,71 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BoardList from "./BoradList";
 import Write from "./Write";
 import Detail from "./Detail";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import axios from "axios";
 
 /**
  * App class
  */
-const App: React.FC = () => {
-  const [date, setDate] = useState("");
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
 
-  const callApi = () => {
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => response.json())
-      .then((json) => {
-        setDate(json.title);
-      });
+const App: React.FC = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null
+  });
+
+  // fetchUsers 함수 정의
+  const fetchUsers = async () => {
+    // 요청 상태(LOADING, SUCCESS, ERROR)에 따라 dispatch 호출
+    // 기본으로 `type: "LOADING"` dispatch가 호출됨
+    dispatch({ type: "LOADING" });
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      dispatch({ type: "SUCCESS", data: response.data });
+    } catch (e) {
+      dispatch({ type: "ERROR", error: e });
+    }
   };
 
   useEffect(() => {
-    // 컴포넌트가 마운트되면 API 호출
-    callApi();
-  }, []); // 빈 배열은 마운트될 때 한 번만 실행됨
+    fetchUsers();
+  }, []);
 
-  // {
-  //   "userId": 1,
-  //   "id": 1,
-  //   "title": "delectus aut autem",
-  //   "completed": false
-  // }
+  const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return null;
 
   return (
     <div className="App">
@@ -44,7 +79,14 @@ const App: React.FC = () => {
             <Route path="/detail/:id" element={<Detail />} />
             <Route path="/" element={<BoardList />} />
           </Routes>
-          <div>{date ? date : "none"}</div>
+          <ul>
+            {users.map((user: any) => (
+              <li key={user.id}>
+                {user.username} ({user.name})
+              </li>
+            ))}
+          </ul>
+          <button onClick={fetchUsers}>불러오기</button>
         </BrowserRouter>
       </div>
     </div>
