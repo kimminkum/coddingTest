88bea618e743b9d98baae47ca8896964c193ead3
kimminkum <100018239+kimminkum@users.noreply.github.com>
2024-01-20 11:12:43 +0900

    rest api kakao

--- react-board/src/Components/KaKaoApi.tsx
+++ react-board/src/Components/KaKaoApi.tsx
@@ -1,5 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
+import * as React from "react";
+import queryString from "query-string";
+import axios from "axios";
+
+interface KakaoTokenData {
+  grant_type: string;
+  client_id: string;
+  redirect_uri: string;
+  code: string;
+}
 
 const KakaoLogin: React.FC = () => {
   const kakaoRestApiKey = "dfa29646980902382513abb967b3a190";
@@ -7,12 +15,73 @@ const KakaoLogin: React.FC = () => {
   const redirectUri = "http://localhost:3000/auth";
   //@ts-ignore
   const Kakao = window.Kakao;
-
-  const [cookies, setCookie] = useCookies(["authorize-access-token"]);
-  const [token, setToken] = useState<string | null>(null);
+  const query = queryString.parse(window.location.search);
 
   const loginWithKakao = () => {
     window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoRestApiKey}&redirect_uri=${redirectUri}&response_type=code`;
+  };
+
+  React.useEffect(() => {
+    if (query.code) {
+      getKakaoTokenHandler(query.code.toString());
+    }
+  }, [query.code]);
+
+  const sendKakaoTokenToServer = async (accessToken: string) => {
+    try {
+      const res = await axios.post("/auth/kakao", {
+        access_token: accessToken
+      });
+
+      if (res.status === 201 || res.status === 200) {
+        const user = res.data.user;
+        window.localStorage.setItem(
+          "token",
+          JSON.stringify({
+            access_token: res.data.jwt
+          })
+        );
+      } else {
+        window.alert("로그인에 실패하였습니다.");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getKakaoTokenHandler = async (code: string) => {
+    try {
+      const dataToSend: KakaoTokenData = {
+        grant_type: "authorization_code",
+        client_id: kakaoRestApiKey,
+        redirect_uri: redirectUri,
+        code: code
+      };
+      const queryStringToSend = Object.keys(dataToSend)
+        .map(
+          (key) =>
+            encodeURIComponent(key) +
+            "=" +
+            encodeURIComponent((dataToSend as any)[key])
+        )
+        .join("&");
+
+      // 토큰 발급 REST API
+      const response = await axios.post(
+        "https://kauth.kakao.com/oauth/token",
+        queryStringToSend,
+        {
+          headers: {
+            "Content-type": "application/x-www-form-urlencoded;charset=utf-8"
+          }
+        }
+      );
+
+      // 서버에 전달
+      sendKakaoTokenToServer(response.data.access_token);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -24,8 +93,6 @@ const KakaoLogin: React.FC = () => {
           alt="카카오 로그인 버튼"
         />
       </a>
-
-      <p id="token-result">{token ? `login success, token: ${token}` : "x"}</p>
     </div>
   );
 };
