c1d087346e89e38846065b111bfd5bce3fbac227
kimminkum <100018239+kimminkum@users.noreply.github.com>
2024-02-01 12:47:49 +0900

    custom map option

--- react-board/src/Components/KakaoMap.tsx
+++ react-board/src/Components/KakaoMap.tsx
@@ -8,6 +8,7 @@ declare global {
 
 const KakaoMap: React.FC = () => {
   const [mapLevel, setMapLevel] = useState<number>(8);
+  const [map, setMap] = useState<any>(null);
 
   useEffect(() => {
     const container = document.getElementById("map");
@@ -16,7 +17,8 @@ const KakaoMap: React.FC = () => {
       level: mapLevel
     };
 
-    const map = new window.kakao.maps.Map(container, options);
+    const initializedMap = new window.kakao.maps.Map(container, options);
+    setMap(initializedMap);
 
     var markerPosition = new window.kakao.maps.LatLng(
       34.365264512305174,
@@ -25,7 +27,7 @@ const KakaoMap: React.FC = () => {
     var marker = new window.kakao.maps.Marker({
       position: markerPosition
     });
-    marker.setMap(map);
+    marker.setMap(initializedMap);
   }, [mapLevel]);
 
   function zoomIn() {
@@ -39,10 +41,59 @@ const KakaoMap: React.FC = () => {
     }
   }
 
+  function setMapType(maptype: string) {
+    var roadmapControl = document.getElementById("btnRoadmap");
+    var skyviewControl = document.getElementById("btnSkyview");
+
+    console.log(roadmapControl);
+    console.log(skyviewControl);
+
+    if (maptype === "roadmap") {
+      map.setMapTypeId(window.kakao.maps.MapTypeId.ROADMAP);
+      roadmapControl?.classList.add("selected_btn");
+      skyviewControl?.classList.remove("selected_btn");
+    } else {
+      map.setMapTypeId(window.kakao.maps.MapTypeId.HYBRID);
+      skyviewControl?.classList.add("selected_btn");
+      roadmapControl?.classList.remove("selected_btn");
+    }
+  }
+
   return (
     <div>
-      <div>
+      <div className="map_wrap">
         <div id="map"></div>
+        <div className="custom_typecontrol radius_border">
+          <span
+            id="btnRoadmap"
+            className="selected_btn"
+            onClick={() => setMapType("roadmap")}
+          >
+            지도
+          </span>
+          <span
+            id="btnSkyview"
+            className="btn"
+            onClick={() => setMapType("skyview")}
+          >
+            스카이뷰
+          </span>
+        </div>
+
+        <div className="custom_zoomcontrol radius_border">
+          <span onClick={zoomIn}>
+            <img
+              src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_plus.png"
+              alt="확대"
+            />
+          </span>
+          <span onClick={zoomOut}>
+            <img
+              src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png"
+              alt="축소"
+            />
+          </span>
+        </div>
         <p>
           <button onClick={zoomIn}>지도레벨 - 1</button>
           <button onClick={zoomOut}>지도레벨 + 1</button>
